23e50d87735b5438afe0e927e7b7745d716a2c70
Kevin <62586828+kcash333@users.noreply.github.com>
2021-07-17 16:52:14 -0500

    Update install_jupyter_server.sh
    http manual bash complete

--- install_jupyter_server.sh
+++ install_jupyter_server.sh
@@ -12,16 +12,21 @@ echo "'sha1:00000000000000000000xxxxxxxxxxxxxxxxxxxxxxxxx'" > sha1.pem
 #Saved the file on local directory
 #Now Generate a config file for server!
 jupyter-notebook --generate-config
-
+#
 nano ~/.jupyter/jupyter_notebook_config.py
-
+#
 echo "insert"
 c = get_config()  # get the config object
 # do not open a browser window by default when using notebooks
 c.NotebookApp.open_browser = False
 # this is the password hash that we generated earlier.
-c.NotebookApp.password = 'sha1:073bb9acaa67:b367308802ab66cb1d7654b6684eafefbd61d004'
-
+c.NotebookApp.password = 'sha1:000000000000xxxxxxxxxxxxxxxxxxxx000000000000'
+#Makes server access via internet 
+c.NotebookApp.ip = '0.0.0.0'
+#
+#You can stop here if you just want http and no domain
+#
+#
 #Install TLS
 sudo wget -r --no-parent -A 'epel-release-*.rpm' https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/
 sudo rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.rpm
@@ -34,7 +39,7 @@ sudo certbot certonly -d py.lurn.cloud
 nano ~/.jupyter/jupyter_notebook_config.py
 c.NotebookApp.certfile = 'fullchain.pem'
 c.NotebookApp.keyfile = 'privkey.pem'
-c.NotebookApp.ip = '0.0.0.0'
+
 
 
 #Copy directory of certbot paths...
